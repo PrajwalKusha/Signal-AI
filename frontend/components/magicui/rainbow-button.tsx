dbf63b6d9c870169d3dd65f6391e69f828257984
Prajwal Kusha <p.kusha@gwu.edu>
2026-02-03 23:21:00 -0500

    Fix Vercel Build: Inline cn helper to avoid import errors

--- frontend/components/magicui/rainbow-button.tsx
+++ frontend/components/magicui/rainbow-button.tsx
@@ -1,6 +1,11 @@
 
 import React from "react";
-import { cn } from "../../lib/utils";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
 
 interface RainbowButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
